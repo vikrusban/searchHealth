5a465ec05b7df7cead69e0443f781b0b351ce3f0
vikrusban <linux.shift47@gmail.com>
2022-03-22 16:14:15 +0500

    Add telegram bot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from time import sleep
 from bs4 import BeautifulSoup
+from config import chat, token
 
 def get_data(url):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36'}
@@ -13,11 +14,11 @@ def get_data(url):
         search_alert = soup.find("div", class_="alert-warning")
 
         if search_alert is not None:
-            print('Товар не найден')
+            #print('Товар не найден')
             sleep(1)
-            return("Товар не найден")
+            return(None)
         else:
-            print(url)
+            #print(url)
             sleep(1)
             return(url)
     elif q.status_code == 404:
@@ -25,16 +26,36 @@ def get_data(url):
         sleep(1)
         return("Запрос заблокирован")
 
+def send_telegram(text: str):
+    url = "https://api.telegram.org/bot"
+    url += token
+    method = url + "/sendMessage"
+
+    r = requests.post(method, data={
+         "chat_id": chat,
+         "text": text
+          })
+
+    if r.status_code != 200:
+        raise Exception("post_text error")
+
 if __name__ == "__main__":
     list_url = []
     with open('list_url.txt') as f:
         list_url = f.read().splitlines()
+    f.close()
 
     while True:
         for x in list_url:
             if x == '':
                 print("Передана пустая строка в файле list_url.txt")
             else:
-                get_data(x)
+                ret = get_data(x)
+                if ret is not None:
+                    print("Тут шлем сообщение в телегу")
+                    send_telegram(ret)
+                    print(ret)
+                else:
+                    print(ret)
         print("Сон на 12 часов")
         sleep(43200)
