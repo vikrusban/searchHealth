d65b3373ceb8b9560ad5b497fb84a7c1184010b6
vikrusban <linux.shift47@gmail.com>
2023-01-24 16:48:15 +0500

    Update libs && update search div block parsing

--- async_collect_data.py
+++ async_collect_data.py
@@ -4,17 +4,17 @@ import aiohttp
 import asyncio
 
 async def collect_data(city):
-    ua = UserAgent()
+    ua = UserAgent(browsers=['edge', 'chrome'])
     headers = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept': 'application/font-woff2;q=1.0,application/font-woff;q=0.9,*/*;q=0.8',
         'User-Agent': ua.random
     }
 
     async with aiohttp.ClientSession() as session:
-        response = await session.get(url='https://farmlend.ru/' + city + '/search?keyword=эуритокс', headers=headers)
+        response = await session.get(url='https://farmlend.ru/'+city+'/search?keyword=эуритокс', headers=headers)
         response.encoding = 'utf-8'
         soup = BeautifulSoup(await response.text(), 'lxml')
-        search_alert = soup.find("div", class_="alert-warning")
+        search_alert = soup.find("div", class_="pi-title")
         if search_alert is not None:
             print('Товар не найден')
         else:
